35a61a17e42fdf6ca34c5affe95f62693b8552b5
Shane <shanekane567@gmail.com>
2025-06-05 03:35:39 +0800

    Added A07

---
+++ ADET/A07/albums.js
@@ -0,0 +1,218 @@
+var albums = {
+    album1: {
+        title: "A Rush of Blood to the Head",
+        link: "https://open.spotify.com/album/0RHX9XECH8IVI3LNgWDpmQ",
+        artist: "Coldplay",
+        description: "A Rush of Blood to the Head is the second studio album by the British rock band Coldplay. It was released on 26 August 2002 by Parlophone in the United Kingdom, and a day later by Capitol in the United States. The album was produced by the band and Ken Nelson, and makes greater use of the electric guitar and piano than the band's debut album, Parachutes (2000).",
+        releaseDate: "26 August 2002",
+        recordedDate: "September 2001 – July 2002",
+        cover: "https://i.scdn.co/image/ab67616d00001e02de09e02aa7febf30b7c02d82",
+        tracks: [
+            "0u35Dpz37TY2M2j20RUdMf",
+            "2nvC4i2aMo4CzRjRflysah",
+            "4hf0hL4kWyjWztZzVsM39V",
+            "75JFxkI2RXiU7L9VXzMkle",
+            "0BCPKOYdS2jbQ8iyB56Zns",
+            "4fP4xTSBli3tKck172LEZ4",
+            "3ou9rSNUQnE7XYmJkUUIOc",
+            "4bPkBHKLKd9WHizsvM2zV3",
+            "7KolrFGhfDi1JTSgQBT5sI",
+            "4JJ5zGKnb1IpERyBrfmb1y",
+            "2uQ4px5SPONsgcUpulywIQ"
+        ]
+    },
+
+    album2: {
+        title: "Parachutes",
+        link: "https://open.spotify.com/album/6ZG5lRT77aJ3btmArcykra",
+        artist: "Coldplay",
+        description: `Parachutes is the debut studio album by the British rock band Coldplay. It was released on 10 July 2000 by Parlophone in the United Kingdom. The album was produced by the band and British record producer Ken Nelson, except for one track, "High Speed", which was produced by Chris Allison. Parachutes spawned the singles "Shiver", "Yellow", and "Trouble", with a limited European release of "Don't Panic". The album was also supported by the Parachutes Tour, which saw the band performing 131 shows in their first world tour.`,
+        releaseDate: "10 July 2000",
+        recordedDate: "November 1999 – May 2000",
+        cover: "https://i.scdn.co/image/ab67616d00001e029164bafe9aaa168d93f4816a",
+        tracks: [
+            "2QhURnm7mQDxBb5jWkbDug",
+            "0qksx8mV28lztYIZ1om8ml",
+            "2mLgOcRkEgq89j8WstUpui",
+            "7D0RhFcb3CrfPuTJ0obrod",
+            "3AJwUDP919kvQ9QcozQPxg",
+            "0R8P9KfGJCDULmlEoBagcO",
+            "4qzoHxgp42ylb18ga1SWTL",
+            "2DHgvPQD1jApRnT1DBZdrS",
+            "5TB6QgrF0RPIxSCGfRDLoe",
+            "1RNtm45kw0hPMBz7gKiIYu"
+        ]
+    },
+
+    album3: {
+        title: "Thriller",
+        link: "https://open.spotify.com/album/2ANVost0y2y52ema1E9xAZ",
+        artist: "Michael Jackson",
+        description: `Thriller is the sixth studio album by the American singer and songwriter Michael Jackson, released on November 29, 1982, by Epic Records. It was produced by Quincy Jones, who previously worked with Jackson on his album Off the Wall (1979). With the ongoing backlash against disco music at the time, he moved in a new musical direction, resulting in a mix of pop, post-disco, rock, funk, synth-pop, and R&B sounds, and darker themes; Jackson wanted to create an album where "every song was a killer". Paul McCartney is the first credited appearance of a featured artist on a Jackson album. Recording took place from April to November 1982 at Westlake Recording Studios in Los Angeles, California, with a budget of $750,000.`,
+        releaseDate: "29 November 1982",
+        recordedDate: "April 14 – November 8, 1982",
+        cover: "https://i.scdn.co/image/ab67616d00001e02de437d960dda1ac0a3586d97",
+        tracks: [
+            "1hu2s7qkm5bo03eODpRQO3",
+            "6XYbMGvtl6tlPoGWaiH7EY",
+            "4IT6vDuKprKl6jyVndlY8V",
+            "2LlQb7Uoj1kKyGhlkBf9aC",
+            "3BovdzfaX4jb5KFQwoPfAw",
+            "7J1uxwnxfQLu4APicE5Rnj",
+            "4cgjA7B4fJBHyB9Ya2bu0t",
+            "1CgmY8fVN7kstVDZmsdM5k",
+            "07L1pzoVerhRSSaDGZHrKy"
+        ]
+    },
+
+    album4: {
+        title: "21st Century Breakdown",
+        link: "https://open.spotify.com/album/1AHZd3C3S8m8fFrhFxyk79",
+        artist: "Green Day",
+        description: `21st Century Breakdown is the eighth studio album by the American rock band Green Day, released on May 15, 2009, through Reprise Records. Green Day commenced work on the record in January 2006 and forty-five songs were written by vocalist and guitarist Billie Joe Armstrong by October 2007, but the band members did not enter studio work until January 2008.`,
+        releaseDate: "15 May 2009",
+        recordedDate: "January 2008 – April 2009",
+        cover: "https://i.scdn.co/image/ab67616d00001e02c2ced39899b0d67cd5a724fa",
+        tracks: [
+            "7v4kWg3dzVd01tknLf853W",
+            "0RT1qESYHlNcUJI0Yo5Hzl",
+            "5qtwzv99vOr5UTwnTixn7j",
+            "575urseJcD6iGioNWuQy3H",
+            "0bziU9XgFkAcPrMhVYBM7n",
+            "0buUlnWLraOX28CQLuR1dE",
+            "5TpPSTItCwtZ8Sltr3vdzm",
+            "0PVplC5P6JJXzEKfIoNTRo",
+            "5jOMxC9ezgFPGSMSn1zJ5S",
+            "5Rjm3VIMlxAEIEy5gU2bF7",
+            "4nqZIPw2PNo1W54kEmswJ2",
+            "2mnk1GHN0XmznKiucY4ZW4",
+            "0CHmnY1V3tmxrIZAkpyxNh",
+            "3Dl2gul5pJ5Ceh7ew5kvC9",
+            "0c0ptxjRaqaxMM8gvSWCG0",
+            "64yrDBpcdwEdNY9loyEGbX",
+            "2DSgsIY3ZCjx7DhMNRzsSS",
+            "5bUXuNEIxQbX6sTiP43z5H"
+        ]
+    },
+
+     album5: {
+        title: "Close to Paradise",
+        link: "https://open.spotify.com/album/7craqx50glaGWMusQ9Ay1O",
+        artist: "Patrick Watson",
+        description: `Close to Paradise is the second studio album by Patrick Watson, released on September 26, 2006. On September 24, 2007, the album won the Polaris Music Prize, after reaching the finals alongside such other albums as Neon Bible (Arcade Fire), Ashtray Rock (Joel Plaskett Emergency), Woke Myself Up (Julie Doiron), and The Reminder (Feist).`,
+        releaseDate: "26 September 2006",
+        recordedDate: "No known date",
+        cover: "https://i.scdn.co/image/ab67616d00001e025039f4c87a6583d206c8bc64",
+        tracks: [
+            "6rgBXqu2GkF3ZfNJBTIiae",
+            "12uVFCaBh15yK9nmRZke4H",
+            "27tWuqondppAdysWo8WpNk",
+            "3c4JhnFZfjhj68bUPMxnOc",
+            "3ZQlHRK6Zglj9cprAgbDYs",
+            "2xea4pIE7gproK31nTNxij",
+            "1L3sQYQoUgv27VnbMFZ53d",
+            "0KNTpVc0TluEx1H3XAptXf",
+            "09LPMbm5ZZikYDCq62pL3r",
+            "0yb5N8veNbmhcn9TjyQNC9",
+            "40ALUAPznjcGK26OezRJcK",
+            "4NnI7qBoRcd5gPqhth2gx8",
+            "18loCBGyZY7KvMeMNZEl2Y"
+        ]
+    },
+
+    album6: {
+        title: "Care",
+        link: "https://open.spotify.com/album/2oMgmFgwsFMrWJFL4WJ14G",
+        artist: "TEMPOREX",
+        description: `TEMPOREX's debut album, Care, was recorded between July and November 2016 in both of his bedrooms. The album was first self-released on November 21, 2016, via Bandcamp and SoundCloud . It was later reissued on May 5, 2018, by Human Sounds Records. The album features a blend of bedroom pop, synthpop, and lo-fi aesthetics, reflecting TEMPOREX's introspective and youthful perspective. Notable tracks include "Nice Boys," "Care," and "Let's Keep It Virtual."`,
+        releaseDate: "21 November 2016",
+        recordedDate: "July – November 2016",
+        cover: "https://i.scdn.co/image/ab67616d00001e0270318591e08f17f2d3eae060",
+        tracks: [
+            "7gkS9SJbH6oMNAhcqsAYXZ",
+            "6uuBSo3wXnosEWVXRzZDTf",
+            "3ceRaomSZjrE7F5LIEgeUc",
+            "0gaOxnHfomT4BwtCYAu19P",
+            "6dBedWHLNERW7BDHibXzwj",
+            "4Jtv4TNTJMcuTELVJWm6Ui",
+            "1bGpUrcboYNTjPCvCuRCCB",
+            "2BP4HKmBoJU9j4PF9zb8Sf",
+            "0voq1uphDsg2LDgGCKUut6",
+            "0v15sQ0FhpWi1TFPXHR0AT",
+            "3iQLwJhYvaQJuGMI22QDad"
+        ]
+    }
+};
+
+function displayAlbums() {
+    var cardContainer = document.getElementById("card-container");
+
+    for (var i = 1; i <= Object.keys(albums).length; i++) {
+        var key = "album" + i;
+        var albumArray = albums[key];
+
+        cardContainer.innerHTML += `
+            <div class="col-12 col-md-4 my-2">
+                <div class="card shadow-light">
+                    <img src="${albumArray.cover}" class="card-img-top"
+                        alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title fw-bold text-truncate">${albumArray.title}</h5>
+                        <p><span class="fw-bold">Artist: </span>${albumArray.artist}</p>
+                        <a href="#album-section" class="btn btn-warning fw-bold"
+                            onclick="loadAlbum('album${i}')"></button>View Album</a>
+                    </div>
+                </div>
+            </div>
+            `;
+    }
+}
+
+function loadAlbum(albumKey) {
+    var album = albums[albumKey];
+    var section = document.getElementById('album-section');
+
+    section.innerHTML = `
+        <div class="row">
+          <div class="d-flex align-items-center justify-content-center col-12 col-md-4 text-center mb-4 mb-md-0">
+            <a href="${album.link}"><img src="${album.cover}" alt="${album.title} Cover" class="img-fluid rounded shadow album-cover"></a>
+          </div>
+          <div class="col-12 col-md-8">
+            <a href="${album.link}" class="text-decoration-none"><h2 class="fw-bold text-primary">${album.title}</h2></a>
+            <h5 class="fw-bold text-primary">- ${album.artist}</h5>
+            <p class="mt-3 text-dark">${album.description}</p>
+            <p class="m-0 text-dark"><span class="fw-bold text-primary">Released:</span> ${album.releaseDate}</p>
+            <p class="m-0 text-dark"><span class="fw-bold text-primary">Recorded:</span> ${album.recordedDate}</p>
+          </div>
+        </div>
+
+        <div class="my-5 bg-dark p-3 rounded-3">
+          <h4 class="fw-bold text-warning mb-3">Tracklist</h4>
+          <div id="tracklist-container"></div>
+        </div>
+
+        <div class="text-center h4 fw-bold text-primary">
+        <p>Curious what day this album was released? Find out now with this cool tool!</p>
+        <p class="text-dark">(Or use it for anything else, actually. :P)</p>
+        </div>
+        <div class="d-flex justify-content-center">
+        <iframe class="mt-3 rounded-3 bg-warning" src="https://shanerhyder.github.io/ADET/A07/embeddable.html" style="width: 50%; height: 33dvh">
+        </iframe>
+        </div>
+      `;
+
+    var tracklistContainer = document.getElementById('tracklist-container');
+    album.tracks.forEach(trackID => {
+        tracklistContainer.innerHTML += `
+                <iframe class="mb-2 track" style="border-radius:12px" src="https://open.spotify.com/embed/track/${trackID}" width="100%" height="80" frameborder="0"
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="eager">
+                </iframe>
+                `
+    });
+
+    var albumSection = document.getElementById("album-section");
+
+    albumSection.classList.add("py-5");
+}
+
+displayAlbums();
